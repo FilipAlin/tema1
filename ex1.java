fd638707f084ffdb4f7da47beb5a5f3361184b40
FilipAlin <76048840+FilipAlin@users.noreply.github.com>
2021-10-02 18:53:03 +0300

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -18,8 +18,8 @@ public class ex1 {
 	}
 	return true;
 	}
-	public static void main(String[] args) { 
-		int [] a= {1,1,0,0,1,0};
+	public static void main(String[] args) {
+		int [] a= {1,1,0,1,1,0};
 		System.out.println(Speciale(a,6));
 	}
 }
